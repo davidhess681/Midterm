763f9a74eed8ccd06a06c01d5c3f9c11cfe25597
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-10-25 14:59:37 -0400

    classes added

---
+++ Midterm/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    class Book
+    {
+        public Book()
+        {
+
+        }
+        public Book(String title, string author, bool status, DateTime dueDate)
+        {
+
+        }
+         
+
+
+
+
+
+
+    }
+}
