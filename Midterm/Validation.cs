2d60ccd88185b5203d735e31c2c04950723f0923
AndreaDabrowski <AnnMDabrowski@gmail.com>
2018-10-25 15:03:54 -0400

    validation added

--- Midterm/Validation.cs
+++ Midterm/Validation.cs
@@ -8,5 +8,29 @@ namespace Midterm
 {
     class Validation
     {
+        //validates the input so that it isnt null or a number
+        public static string IsInputValid(string info)
+        {
+            while (true)
+            {
+                if (string.IsNullOrEmpty(info) || info.Length > 35)
+                {
+                    Console.WriteLine("That is not correct input, try again");
+
+                }
+
+                foreach (char item in info)
+                {
+                    if (!char.IsLetter(item))
+                    {
+                        Console.WriteLine("You must use alphabetical characters, try again ");
+
+                    }
+                }
+
+                return info;
+            }
+
+        }
     }
 }
